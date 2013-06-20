a8e958b16365b88c84a9b4649351668a9acc6e61
Vitor HP <vitorhp2@gmail.com>
2013-06-19 22:58:07 -0300

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RpgRules::Application.config.secret_token = 'f0a5ddb43c8e7ff5beb2e1403c00c93a4da7b2188aec6d88e0e04d21c57c02521e5e9c9f6a7277386bbaf2e534fce87a66921ccaf1a3725f3badaf7eeaf52b8c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+RpgRules::Application.config.session_store :cookie_store, key: '_rpg_rules_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# RpgRules::Application.config.session_store :active_record_store
